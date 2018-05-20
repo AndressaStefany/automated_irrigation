1a9e04d16b59753c24bb3a68db1fe491ef1b9bbf
AndressaStefany <astefanysoliveira@gmail.com>
2018-05-20 20:32:29 -0300

    validação dos inputs

--- interfaceWEB/form.js
+++ interfaceWEB/form.js
@@ -1,86 +1,107 @@
+
+// Starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+        }, false);
+    });
+    }, false);
+})();
+
+
 function campos(){
     var e = document.getElementById("formControlSelect1");
     var modo = e.options[e.selectedIndex].value;
 
     if(modo == 0){
-        $("#minutos").attr("disabled", "disabled").val("");
-        $("#time").attr("disabled", "disabled").val("");
-        $("#umi_min").attr("disabled", "disabled").val("");
-        $("#umi_max").attr("disabled", "disabled").val("");
-        $("#temp_min").attr("disabled", "disabled").val("");
-        $("#temp_max").attr("disabled", "disabled").val("");
+        $("#minutos").attr("disabled", "disabled").val("").prop("required",false);
+        $("#time").attr("disabled", "disabled").val("").prop("required",false);
+        $("#umi_min").attr("disabled", "disabled").val("").prop("required",false);
+        $("#umi_max").attr("disabled", "disabled").val("").prop("required",false);
+        $("#temp_min").attr("disabled", "disabled").val("").prop("required",false);
+        $("#temp_max").attr("disabled", "disabled").val("").prop("required",false);
         $("#cadastrar").attr("disabled", "disabled");
     }
     else if(modo == 1){
         // enable
-        $("#minutos").removeAttr("disabled");
-        $("#time").removeAttr("disabled");
+        $("#minutos").removeAttr("disabled").prop("required",true);
+        $("#time").removeAttr("disabled").prop("required",true);
         $("#cadastrar").removeAttr("disabled");
 
         // disabled
-        $("#umi_min").attr("disabled", "disabled").val("");
-        $("#umi_max").attr("disabled", "disabled").val("");
-        $("#temp_min").attr("disabled", "disabled").val("");
-        $("#temp_max").attr("disabled", "disabled").val("");
+        $("#umi_min").attr("disabled", "disabled").val("").prop("required",false);
+        $("#umi_max").attr("disabled", "disabled").val("").prop("required",false);
+        $("#temp_min").attr("disabled", "disabled").val("").prop("required",false);
+        $("#temp_max").attr("disabled", "disabled").val("").prop("required",false);
     }
     else if(modo == 2){
         // enable
-        $("#umi_min").removeAttr("disabled");
-        $("#umi_max").removeAttr("disabled");
+        $("#umi_min").removeAttr("disabled").prop("required",true);
+        $("#umi_max").removeAttr("disabled").prop("required",true);
         $("#cadastrar").removeAttr("disabled");
 
         // disabled
-        $("#minutos").attr("disabled", "disabled").val("");
-        $("#time").attr("disabled", "disabled").val("");
-        $("#temp_min").attr("disabled", "disabled").val("");
-        $("#temp_max").attr("disabled", "disabled").val("");
+        $("#minutos").attr("disabled", "disabled").val("").prop("required",false);
+        $("#time").attr("disabled", "disabled").val("").prop("required",false);
+        $("#temp_min").attr("disabled", "disabled").val("").prop("required",false);
+        $("#temp_max").attr("disabled", "disabled").val("").prop("required",false);
     }
     else if(modo == 3){
         // enable
-        $("#umi_min").removeAttr("disabled");
-        $("#minutos").removeAttr("disabled");
+        $("#umi_min").removeAttr("disabled").prop("required",true);
+        $("#minutos").removeAttr("disabled").prop("required",true);
         $("#cadastrar").removeAttr("disabled");
 
         // disabled
-        $("#time").attr("disabled", "disabled").val("");
-        $("#umi_max").attr("disabled", "disabled").val("");
-        $("#temp_min").attr("disabled", "disabled").val("");
-        $("#temp_max").attr("disabled", "disabled").val("");
+        $("#time").attr("disabled", "disabled").val("").prop("required",false);
+        $("#umi_max").attr("disabled", "disabled").val("").prop("required",false);
+        $("#temp_min").attr("disabled", "disabled").val("").prop("required",false);
+        $("#temp_max").attr("disabled", "disabled").val("").prop("required",false);
     }
     else if(modo == 4){
         // enable
-        $("#temp_min").removeAttr("disabled");
-        $("#temp_max").removeAttr("disabled");
+        $("#temp_min").removeAttr("disabled").prop("required",true);
+        $("#temp_max").removeAttr("disabled").prop("required",true);
         $("#cadastrar").removeAttr("disabled");
 
         // disabled
-        $("#minutos").attr("disabled", "disabled").val("");
-        $("#time").attr("disabled", "disabled").val("");
-        $("#umi_min").attr("disabled", "disabled").val("");
-        $("#umi_max").attr("disabled", "disabled").val("");
+        $("#minutos").attr("disabled", "disabled").val("").prop("required",false);
+        $("#time").attr("disabled", "disabled").val("").prop("required",false);
+        $("#umi_min").attr("disabled", "disabled").val("").prop("required",false);
+        $("#umi_max").attr("disabled", "disabled").val("").prop("required",false);
     }
     else if(modo == 5){
         // enable
-        $("#minutos").removeAttr("disabled");
-        $("#temp_max").removeAttr("disabled");
+        $("#minutos").removeAttr("disabled").prop("required",true);
+        $("#temp_max").removeAttr("disabled").prop("required",true);
         $("#cadastrar").removeAttr("disabled");
 
         // disabled
-        $("#time").attr("disabled", "disabled").val("");
-        $("#umi_min").attr("disabled", "disabled").val("");
-        $("#umi_max").attr("disabled", "disabled").val("");
-        $("#temp_min").attr("disabled", "disabled").val("");
+        $("#time").attr("disabled", "disabled").val("").prop("required",false);
+        $("#umi_min").attr("disabled", "disabled").val("").prop("required",false);
+        $("#umi_max").attr("disabled", "disabled").val("").prop("required",false);
+        $("#temp_min").attr("disabled", "disabled").val("").prop("required",false);
     }
     else if(modo == 6){
         // enable
-        $("#temp_min").removeAttr("disabled");
-        $("#temp_max").removeAttr("disabled");
-        $("#umi_min").removeAttr("disabled");
-        $("#umi_max").removeAttr("disabled");
+        $("#temp_min").removeAttr("disabled").prop("required",true);
+        $("#temp_max").removeAttr("disabled").prop("required",true);
+        $("#umi_min").removeAttr("disabled").prop("required",true);
+        $("#umi_max").removeAttr("disabled").prop("required",true);
         $("#cadastrar").removeAttr("disabled");
 
         // disabled
-        $("#minutos").attr("disabled", "disabled").val("");
-        $("#time").attr("disabled", "disabled").val("");
+        $("#minutos").attr("disabled", "disabled").val("").prop("required",false);
+        $("#time").attr("disabled", "disabled").val("").prop("required",false);
     }
 }
