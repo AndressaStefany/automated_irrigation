64fa8e7199ae1f8d6bde104145292049589b7f17
Vitor Ramos <ramos.vitor89@gmail.com>
2018-06-03 23:30:41 -0300

    error handler

--- raspberry/client.py
+++ raspberry/client.py
@@ -2,28 +2,22 @@ from nodemcu import node
 from simplehttpserver import server
 import psycopg2
 
-print('starting PostgreSQL connection')
 try:
+    print('Starting PostgreSQL connection')
     con = psycopg2.connect(host='localhost', database='nodemcu', user='postgres', password='admin')
-    #cur = con.cursor()
-except psycopg2.Error as e:
-    print("Cannot connect to database", e)
-    exit(0)
 
-print('starting nodemcu')
-try:
+    print('Starting nodemcu')
     node = node.Node(con)
-except:
-    print("Error on create node instance")
-    exit(0)
 
-print('starting server...')
-try:
+    print('Starting server...')
     server_address = ('', 8081)
-    serv= server.Server(server_address,'interfaceWEB', node)
+    serv = server.Server(server_address, 'interfaceWEB', node)
+
+    print('Running server...')
+    serv.run()
+except psycopg2.Error as e:
+    print('Cannot connect to database', e)
+except node.Error as e:
+    print('Error on create node instance', e)
 except:
     print("Error on create http server")
-    exit(0)
-
-serv.run()
-print('running server...')
--- raspberry/nodemcu/node.py
+++ raspberry/nodemcu/node.py
@@ -4,23 +4,29 @@ import time
 from threading import Thread
 from datetime import datetime
 
+class Error(Exception):
+    def __init__(self, value):
+        self.value= value
+    def __str__(self):
+        return repr(self.value)
+
 class Node:
     def __init__(self, db):
+        self.verbose= 1
         self.server_adddress = ('192.168.0.111', 5780)
         self.connected= False
         #self.sock= None
         self.con = db
-        self.connect_node()
         try:
-            self.t1= Thread(target=self.recv_data)
+            self.t1= Thread(target=self.reconnect)
             self.t1.start()
-            self.t2 = Thread(target=self.reconnect)
+            self.t2 = Thread(target=self.recv_data)
             self.t2.start()
         except:
-            print('Error on creating recv thread')
+            raise Error('Error on creating threads')
 
     def connect_node(self):
-        print('Attempt to connect {} port {}'.format(*self.server_adddress))
+        if self.verbose: print('Attempt to connect {} port {}'.format(*self.server_adddress))
         try:
             # Create a TCP/IP socket
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,9 +34,9 @@ class Node:
 
             self.sock.send(b'ok')
             self.connected= True
-            print('Successfully connected')
+            if self.verbose: print('Successfully connected')
         except socket.error as e:
-            print('Error on attempt to connect', e)
+            if self.verbose: print('Error on attempt to connect', e)
             self.connected = False
 
         self.send_last_mode()
@@ -54,35 +60,44 @@ class Node:
             modo = [last_r[pars['modo']], last_r[pars['temp_min']], last_r[pars['temp_max']], last_r[pars['umi_min']],
                     last_r[pars['umi_max']]]
 
-
+        if self.verbose > 1: print('Seding last mode and time')
         # wait for the nodemcu
         time.sleep(0.5)
         for i in range(0,4):
             self.send_data(modo)
             minute= datetime.now()
-            self.send_data([127, (minute.hour*60+minute.minute)*10+minute.second/6.0, 0])
+            timepack= [127, (minute.hour*60+minute.minute)*10+minute.second/6.0, 0]
+            self.send_data(timepack)
             time.sleep(0.1)
 
     def reconnect(self):
+        while not self.connected:
+            self.connect_node()
+            time.sleep(5)
         while 1:
-            #print("Verificando conexao")
+            if self.verbose > 2: print("Verificando conexao")
             try:
                 data = b'ok'
                 self.sock.send(data)
                 self.connected = True
             except:
-                print('Reconnecting...')
+                if self.verbose: print('Reconnecting...')
                 self.connected = False
                 self.sock.close()
-                self.connect_node()
+                while not self.connected:
+                    self.connect_node()
+                    time.sleep(5)
             time.sleep(5)
 
     def recv_data(self):
+        while not self.connected:
+            pass
         while 1:
             try:
                 data = self.sock.recv(8)
+                if self.verbose > 1: print('Recv ', data, struct.unpack('ff', data))
             except:
-                print('Connection lost -- recv')
+                if self.verbose: print('Connection lost -- recv')
                 self.connected= False
                 self.sock.close()
                 time.sleep(5)
@@ -94,9 +109,10 @@ class Node:
             try:
                 modo_= [int(x) for x in modo]
                 data = struct.pack("<B"+"H"*(len(modo)-1), *modo_)
+                if self.verbose > 1: print('Seding ', data, modo_)
                 self.sock.send(data)
             except:
-                print('Connection lost -- send')
+                if self.verbose: print('Connection lost -- send')
                 self.connected = False
                 self.sock.close()
 
@@ -104,10 +120,11 @@ class Node:
         try:
             sql= 'select {} from {} where {}'.format(cols,table,where)
             cur= self.con.cursor()
+            if self.verbose > 1: print(sql)
             cur.execute(sql)
             return cur.fetchall()
         except:
-            print("Error on geting data ")
+            if self.verbose: print("Error on geting data ")
 
     def save_data(self, table, keys, val):
         try:
@@ -116,8 +133,10 @@ class Node:
             for k,v in zip(keys,val):
                 keys_+=k+(',' if k != keys[-1] else '')
                 val_+=str(v)+(',' if k != keys[-1] else '')
-            sql = 'insert into {} ({}) values ({})'.format(table,keys_,val_)
-            self.con.cursor().execute(sql)
+#            sql = 'insert into {} ({}) values ({})'.format(table,keys_,val_)
+            sql = 'insert into {} ({}) values ({})'.format(table,keys_,'%s,'*(len(val)-1)+'%s')
+            if self.verbose > 1: print(sql,val)
+            self.con.cursor().execute(sql,tuple(val))
             self.con.commit()
         except:
-            print('Error on saving on database in table {}'.format(table))
+            if self.verbose: print('Error on saving on database in table {}'.format(table))
